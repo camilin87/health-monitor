1558e64f48a6beedd7bd2dc12be0941b95b6b0e9
Camilo Sanchez <camilin87@gmail.com>
2017-08-26 18:04:45 -0400

    Create a constraint so that db columns are required Also, the table name needs to be an atom instead of a string, this is due to the older ecto version

--- hm_server/priv/repo/migrations/20170826134019_create_api_credentials.exs
+++ hm_server/priv/repo/migrations/20170826134019_create_api_credentials.exs
@@ -2,14 +2,14 @@ defmodule HMServer.Repo.Migrations.CreateApiCredentials do
   use Ecto.Migration
 
   def change do
-    create table("api_credentials") do
-      add :client_id, :string, size: 256
-      add :secret_key, :string, size: 256
+    create table(:api_credentials) do
+      add :client_id, :string, size: 256, null: false
+      add :secret_key, :string, size: 256, null: false
       add :client_disabled, :boolean, default: false
 
       timestamps()
     end
 
-    create index("api_credentials", [:client_id], unique: true)
+    create index(:api_credentials, [:client_id], unique: true)
   end
 end
