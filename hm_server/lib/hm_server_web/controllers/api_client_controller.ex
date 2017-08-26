94b3f00e50930842d1c8e2e22d422b0e6b4c6396
Camilo Sanchez <camilin87@gmail.com>
2017-08-26 19:50:24 -0400

    minor refactor to make the code more elixir idiomatic

--- hm_server/lib/hm_server_web/controllers/api_client_controller.ex
+++ hm_server/lib/hm_server_web/controllers/api_client_controller.ex
@@ -2,8 +2,8 @@ defmodule HMServerWeb.ApiClientController do
   use HMServerWeb, :controller
 
   def index(conn, _params) do
-    credentials = HMServer.Repo.all(HMServer.Credential)
-    json_result = Enum.map(credentials, &credential_json/1)
+    json_result = HMServer.Repo.all(HMServer.Credential)
+    |> Enum.map(&credential_json/1)
 
     json conn, json_result
   end
