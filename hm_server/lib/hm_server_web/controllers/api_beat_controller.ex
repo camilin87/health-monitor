ce9b1a04a05d2e504e7416c54a684bbfaa42aa89
Camilo Sanchez <camilin87@gmail.com>
2017-08-26 19:58:28 -0400

    start the credentials validation

--- hm_server/lib/hm_server_web/controllers/api_beat_controller.ex
+++ hm_server/lib/hm_server_web/controllers/api_beat_controller.ex
@@ -2,6 +2,8 @@ defmodule HMServerWeb.ApiBeatController do
   use HMServerWeb, :controller
 
   def create(conn, _params) do
-    json conn, %{success: true}
+    # json conn, %{success: true}
+
+    send_resp(conn, :unauthorized, "")
   end
 end
--- hm_server/test/hm_server_web/controllers/api_beat_controller_test.exs
+++ hm_server/test/hm_server_web/controllers/api_beat_controller_test.exs
@@ -1,9 +1,18 @@
 defmodule HMServerWeb.ApiBeatControllerTest do
   use HMServerWeb.ConnCase
 
-  test "POST /api/beat", %{conn: conn} do
+  test "POST /api/beat fails when no credentials are provided", %{conn: conn} do
     conn = post conn, "/api/beat"
 
-    assert json_response(conn, 200) == %{"success" => true}
+    assert conn.status == 401
   end
+
+  #
+  # this test needs to be enhanced
+  #
+  # test "POST /api/beat", %{conn: conn} do
+  #   conn = post conn, "/api/beat"
+
+  #   assert json_response(conn, 200) == %{"success" => true}
+  # end
 end
