e166dc883dcd4d28cc960c0dff596ad9ac119b7c
Camilo Sanchez <camilin87@gmail.com>
2017-08-30 19:48:55 -0400

    extract decode_user for clarity

--- hm_server/lib/hm_server_web/authorization.ex
+++ hm_server/lib/hm_server_web/authorization.ex
@@ -3,13 +3,16 @@ defmodule HMServerWeb.Authorization do
 
   def read_user!(conn) do
     case get_req_header(conn, "authorization") do
-      ["Basic " <> encoded_kvp] ->
-          case encoded_kvp |> Base.decode64! |> String.split(":") do
-            ["", _] -> raise ArgumentError, "The `authorization` header contains an empty user."
-            [user, _] -> user
-            _ -> raise ArgumentError, "The `authorization` header does not contain a valid user."
-          end
+      ["Basic " <> encoded_kvp] -> decode_user(encoded_kvp)
       _ -> raise ArgumentError, "The `authorization` header in the request is missing or it has an incorrect value."
+    end
+  end
+
+  defp decode_user(encoded_kvp) do
+    case encoded_kvp |> Base.decode64! |> String.split(":") do
+      ["", _] -> raise ArgumentError, "The `authorization` header contains an empty user."
+      [user, _] -> user
+      _ -> raise ArgumentError, "The `authorization` header does not contain a valid user."
     end
   end
 end
