f3fecee9fdb382e52491cff34a904d0e3517e82a
Camilo Sanchez <camilin87@gmail.com>
2017-08-30 19:46:36 -0400

    get rid of the non-throwing read_user method

--- hm_server/lib/hm_server_web/authorization.ex
+++ hm_server/lib/hm_server_web/authorization.ex
@@ -1,22 +1,6 @@
 defmodule HMServerWeb.Authorization do
   import Plug.Conn
 
-  def read_user(conn) do
-    case get_req_header(conn, "authorization") do
-      ["Basic " <> encoded_header] ->
-        case Base.decode64(encoded_header) do
-          {:ok, decoded_header} ->
-              case String.split(decoded_header, ":") do
-                ["", _] -> :error
-                [user, _] -> {:ok, user}
-                _ -> :error
-              end
-          _ -> :error
-        end
-      _ -> :error
-    end
-  end
-
   def read_user!(conn) do
     case get_req_header(conn, "authorization") do
       ["Basic " <> encoded_kvp] ->
--- hm_server/test/hm_server_web/authorization_test.exs
+++ hm_server/test/hm_server_web/authorization_test.exs
@@ -1,28 +1,6 @@
 defmodule HMServerWeb.AuthorizationTest do
   use HMServerWeb.ConnCase
 
-  describe "read_user" do
-    test "reads the user", %{conn: conn} do
-      assert {:ok, default_user()} == conn |> authenticate |> HMServerWeb.Authorization.read_user
-    end
-
-    test "doesn't read the user when it is empty", %{conn: conn} do
-      assert :error == conn |> authenticate("") |> HMServerWeb.Authorization.read_user
-    end
-
-    test "doesn't read the user when no authorization header is provided", %{conn: conn} do
-      assert :error == conn |> HMServerWeb.Authorization.read_user
-    end
-
-    test "doesn't read the user when authorization is not basic", %{conn: conn} do
-      assert :error == conn |> put_req_header("authorization", "invalid") |> HMServerWeb.Authorization.read_user
-    end
-
-    test "doesn't read the user when header value is invalid", %{conn: conn} do
-      assert :error == conn |> put_req_header("authorization", "Basic invalid") |> HMServerWeb.Authorization.read_user
-    end
-  end
-
   describe "read_user!" do
     test "reads the user", %{conn: conn} do
       assert default_user() == conn |> authenticate |> HMServerWeb.Authorization.read_user!
