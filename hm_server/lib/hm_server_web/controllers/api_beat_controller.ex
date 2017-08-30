8f4d35200ce0c8690de68e93a9a9c0ba4d7ea179
Camilo Sanchez <camilin87@gmail.com>
2017-08-30 19:51:28 -0400

    refactor the api_beat_controller to use the authorization module

--- hm_server/lib/hm_server_web/controllers/api_beat_controller.ex
+++ hm_server/lib/hm_server_web/controllers/api_beat_controller.ex
@@ -6,23 +6,13 @@ defmodule HMServerWeb.ApiBeatController do
   ] when action in [:create]
 
   def create(conn, %{"hostname" => hostname}) do
-    # HMServer.Credential |> HMServer.Repo.all |> IO.inspect
-    # IO.puts "-------"
-
-    header_content = Plug.Conn.get_req_header(conn, "authorization")
-    ["Basic " <> encoded_string] = header_content
-    decoded_string = encoded_string |> Base.decode64! |> String.split(":")
-    [client_id, _] = decoded_string
-    # IO.inspect client_id
-    # IO.puts "-------"
-
-    cred = HMServer.Credential |> HMServer.Repo.get_by!(client_id: client_id)
-    # IO.inspect cred
+    request_client_id = HMServerWeb.Authorization.read_user!(conn)
+    credential = HMServer.Credential |> HMServer.Repo.get_by!(client_id: request_client_id)
 
     %HMServer.Node {
       hostname: hostname,
       last_beat: DateTime.utc_now,
-      credential: cred
+      credential: credential
     } |> HMServer.Repo.insert!
 
     json conn, %{success: true}
