ada6e869cc81e642f32d8a3e1af84ab5c43bac1d
Camilo Sanchez <camilin87@gmail.com>
2017-08-30 19:41:47 -0400

    implement the read_user! method

--- hm_server/lib/hm_server_web/authorization.ex
+++ hm_server/lib/hm_server_web/authorization.ex
@@ -16,4 +16,16 @@ defmodule HMServerWeb.Authorization do
       _ -> :error
     end
   end
+
+  def read_user!(conn) do
+    case get_req_header(conn, "authorization") do
+      ["Basic " <> encoded_kvp] ->
+          case encoded_kvp |> Base.decode64! |> String.split(":") do
+            ["", _] -> raise ArgumentError, "The `authorization` header contains an empty user."
+            [user, _] -> user
+            _ -> raise ArgumentError, "The `authorization` header does not contain a valid user."
+          end
+      _ -> raise ArgumentError, "The `authorization` header in the request is missing or it has an incorrect value."
+    end
+  end
 end
--- hm_server/test/hm_server_web/authorization_test.exs
+++ hm_server/test/hm_server_web/authorization_test.exs
@@ -22,4 +22,34 @@ defmodule HMServerWeb.AuthorizationTest do
       assert :error == conn |> put_req_header("authorization", "Basic invalid") |> HMServerWeb.Authorization.read_user
     end
   end
+
+  describe "read_user!" do
+    test "reads the user", %{conn: conn} do
+      assert default_user() == conn |> authenticate |> HMServerWeb.Authorization.read_user!
+    end
+
+    test "fails when the user is empty", %{conn: conn} do
+      assert_raise ArgumentError, fn ->
+        assert default_user() == conn |> authenticate("") |> HMServerWeb.Authorization.read_user!
+      end
+    end
+
+    test "fails when no authorization header is provided", %{conn: conn} do
+      assert_raise ArgumentError, fn ->
+        conn |> HMServerWeb.Authorization.read_user!
+      end
+    end
+
+    test "fails when authorization is not basic", %{conn: conn} do
+      assert_raise ArgumentError, fn ->
+        conn |> put_req_header("authorization", "invalid") |> HMServerWeb.Authorization.read_user!
+      end
+    end
+
+    test "fails when header value is invalid", %{conn: conn} do
+      assert_raise ArgumentError, fn ->
+        conn |> put_req_header("authorization", "Basic invalid") |> HMServerWeb.Authorization.read_user!
+      end
+    end
+  end
 end
