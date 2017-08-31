ffb6fc32415a5a5ac0c841f516dbd38f231938f7
Camilo Sanchez <camilin87@gmail.com>
2017-08-30 22:22:39 -0400

    refactor the code to use inser_or_update

--- hm_server/lib/hm_server_web/controllers/api_beat_controller.ex
+++ hm_server/lib/hm_server_web/controllers/api_beat_controller.ex
@@ -10,21 +10,21 @@ defmodule HMServerWeb.ApiBeatController do
     credential = HMServer.Credential |> HMServer.Repo.get_by!(client_id: request_client_id)
 
     query = %{hostname: hostname, credential_id: credential.id}
-    case HMServer.Node |> HMServer.Repo.get_by(query) do
-      nil ->
-        %HMServer.Node {
-          hostname: hostname,
-          last_beat: DateTime.utc_now,
-          credential: credential
-        } |> HMServer.Repo.insert!
-      %HMServer.Node{node_disabled: true} -> nil
-      node ->
-        updated_values = %{failure_count: 0, last_beat: DateTime.utc_now}
-        updated_fields = [:failure_count, :last_beat]
+    node = case HMServer.Node |> HMServer.Repo.get_by(query) do
+      nil -> %HMServer.Node {hostname: hostname, credential: credential}
+      %HMServer.Node{node_disabled: true} -> :skip
+      existing_node -> existing_node
+    end
 
-        node
-        |> Ecto.Changeset.cast(updated_values, updated_fields)
-        |> HMServer.Repo.update!
+    case node do
+      :skip -> nil
+      _ ->
+        changes = %{failure_count: 0, last_beat: DateTime.utc_now}
+        fieldnames = [:failure_count, :last_beat]
+
+        node 
+        |> Ecto.Changeset.cast(changes, fieldnames)
+        |> HMServer.Repo.insert_or_update!
     end
 
     json conn, %{success: true}
