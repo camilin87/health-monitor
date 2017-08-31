5669f1a4b12a38819cd2e43074f21ec1cea70ec5
Camilo Sanchez <camilin87@gmail.com>
2017-08-30 20:25:37 -0400

    implement Credential.is_valid

--- hm_server/lib/hm_server/credentials.ex
+++ hm_server/lib/hm_server/credentials.ex
@@ -8,4 +8,16 @@ defmodule HMServer.Credential do
 
     timestamps()
   end
+
+  def is_valid("", ""), do: false
+  def is_valid("", _), do: false
+  def is_valid(_, ""), do: false
+  def is_valid(user, secret) do
+    query = %{client_id: user, secret_key: secret, client_disabled: false}
+
+    case HMServer.Repo.get_by(HMServer.Credential, query) do
+      %HMServer.Credential{} -> true
+      _ -> false
+    end
+  end
 end
---
+++ hm_server/test/hm_server/credentials_test.exs
@@ -0,0 +1,37 @@
+defmodule HMServer.CredentialTest do
+  use HMServerWeb.ConnCase
+
+  import HMServer.Credential
+
+  describe "is_valid" do
+    test "false when incomplete credentials provided" do
+      assert false == is_valid("", "")
+      assert false == is_valid("", "secret")
+      assert false == is_valid("user", "")
+    end
+
+    test "false when there are no credentials in the db" do
+      assert false == is_valid("user", "pwd")
+    end
+
+    test "false when the credentials are incorrect" do
+      insert!(:credential)
+
+      assert false == is_valid("unknown", "unknown")
+      assert false == is_valid(default_user(), "unknown")
+      assert false == is_valid("unknown", default_password())
+    end
+
+    test "false when the credential is disabled" do
+      insert!(:credential, %{client_disabled: true})
+
+      assert false == is_valid(default_user(), default_password())
+    end
+
+    test "true when the credentials are correct" do
+      insert!(:credential)
+
+      assert true == is_valid(default_user(), default_password())
+    end
+  end
+end
