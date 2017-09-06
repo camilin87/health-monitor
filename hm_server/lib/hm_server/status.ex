9b250025b0a0c55e0878a47b462d440dfd4456df
Camilo Sanchez <camilin87@gmail.com>
2017-09-05 22:32:23 -0400

    create the server status controller

--- hm_server/lib/hm_server/status.ex
+++ hm_server/lib/hm_server/status.ex
@@ -4,10 +4,19 @@ defmodule HMServer.Status do
   """
 
   use Ecto.Schema
+  alias HMServer.Repo, as: Repo
 
   schema "api_status" do
     field :disabled, :boolean
 
     timestamps()
+  end
+
+  def online? do
+    case HMServer.Status |> Repo.one do
+      nil -> false
+      %HMServer.Status{disabled: true} -> false
+      _ -> true
+    end
   end
 end
--- hm_server/lib/hm_server_web/controllers/api_status_controller.ex
+++ hm_server/lib/hm_server_web/controllers/api_status_controller.ex
@@ -1,7 +1,17 @@
 defmodule HMServerWeb.ApiStatusController do
   use HMServerWeb, :controller
+  alias HMServer.Status, as: Status
 
   def index(conn, _params) do
-    json conn, %{success: true, timestamp: DateTime.utc_now}
+    service_online = Status.online?
+    service_status = case service_online do
+      true -> :ok
+      _ -> :service_unavailable
+    end
+
+    conn
+        |> put_status(service_status)
+        |> json(%{success: service_online, timestamp: DateTime.utc_now})
+        |> halt
   end
 end
