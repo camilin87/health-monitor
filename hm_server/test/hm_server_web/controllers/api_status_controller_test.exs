5103db0bdf39a6888fc5f202671de3217edc0aea
Camilo Sanchez <camilin87@gmail.com>
2017-09-06 12:10:06 -0400

    validate the status page returns not ok when the db is not online

--- hm_server/test/hm_server_web/controllers/api_status_controller_test.exs
+++ hm_server/test/hm_server_web/controllers/api_status_controller_test.exs
@@ -1,8 +1,22 @@
 defmodule HMServerWeb.ApiStatusControllerTest do
   use HMServerWeb.ConnCase
+  alias HMServer.Repo, as: Repo
 
   test "GET /api/status returns ok", %{conn: conn} do
+    Cachex.clear(:api_cache)
+
     conn = get conn, "/api/status"
+
     assert true == json_response(conn, 200)["success"]
+  end
+
+  test "GET /api/status returns not ok the db is not online", %{conn: conn} do
+    Cachex.clear(:api_cache)
+    HMServer.Status
+    |> Repo.delete_all
+
+    conn = get conn, "/api/status"
+
+    assert false == json_response(conn, 503)["success"]
   end
 end
