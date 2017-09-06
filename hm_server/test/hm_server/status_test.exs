0f607389cc0ebb93d10bba4317de386b14860637
Camilo Sanchez <camilin87@gmail.com>
2017-09-05 22:55:23 -0400

    first attempt at testing the status model

---
+++ hm_server/test/hm_server/status_test.exs
@@ -0,0 +1,35 @@
+defmodule HMServer.StatusTest do
+  use HMServerWeb.ConnCase
+
+  alias HMServer.Status, as: Status
+  alias HMServer.Repo, as: Repo
+
+  describe "online?" do
+    test "returns false when db is empty" do
+      HMServer.Status
+      |> Repo.delete_all
+
+      assert false == Status.online?
+    end
+
+    test "returns false when the service is disabled" do
+      HMServer.Status
+      |> Repo.delete_all
+
+      %HMServer.Status{disabled: true}
+      |> Repo.insert!
+
+      assert false == Status.online?
+    end
+
+    test "returns true when the service is enabled" do
+      HMServer.Status
+      |> Repo.delete_all
+
+      %HMServer.Status{disabled: false}
+      |> Repo.insert!
+
+      assert true == Status.online?
+    end
+  end
+end
