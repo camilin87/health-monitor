5b4ac163fbd8df24beb938d2478e812065038380
Camilo Sanchez <camilin87@gmail.com>
2017-09-05 22:03:17 -0400

    the status page will return when it was rendered

--- hm_server/lib/hm_server_web/controllers/api_status_controller.ex
+++ hm_server/lib/hm_server_web/controllers/api_status_controller.ex
@@ -2,6 +2,6 @@ defmodule HMServerWeb.ApiStatusController do
   use HMServerWeb, :controller
 
   def index(conn, _params) do
-    json conn, %{success: true}
+    json conn, %{success: true, timestamp: DateTime.utc_now}
   end
 end
