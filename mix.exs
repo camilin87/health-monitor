80053b2b394bce9e2e9f889fd4dcc758d52a3aef
Camilo Sanchez <camilin87@gmail.com>
2017-09-14 19:13:37 -0400

    make it easier to seed some credentials

--- mix.exs
+++ mix.exs
@@ -55,8 +55,10 @@ defmodule HMServer.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "ecto.seed": ["run priv/repo/seeds.exs"],
+      "seed": ["ecto.seed"],
       "test": ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,14 +1,7 @@
 # Script for populating the database. You can run it as:
 #
-#     mix run priv/repo/seeds.exs
+#     mix seed
 #
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     HMServer.Repo.insert!(%HMServer.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
 
 defmodule SeedHelper do
   def get_credential(name), do: HMServer.Repo.get_by(HMServer.Credential, client_id: name)
