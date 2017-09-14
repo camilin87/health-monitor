1f1a41fe266b71914b5e3a9f4859bfdbc01478f9
Camilo Sanchez <camilin87@gmail.com>
2017-09-14 08:38:39 -0400

    correct copy-paste error, also no need to pass the DATABASE_URL in the config because the repo already does that

--- config/prod.exs
+++ config/prod.exs
@@ -23,9 +23,8 @@ config :hm_server, HMServerWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :hello, Hello.Repo,
+config :hello, HMServer.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
