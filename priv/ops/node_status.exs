9ae7b4b6515e1a96ba31601c44a8022aefab10b6
Camilo Sanchez <camilin87@gmail.com>
2017-09-15 17:13:45 -0400

    implement a command to enable/disable a node

---
+++ priv/ops/node_status.exs
@@ -0,0 +1,33 @@
+# Script to enable/disable a Node
+# It can be run like this
+#    mix ops.node.enable --client-id test --node node1
+#    mix ops.node.disable --client-id test --node node1
+#
+
+require Logger
+# alias HMServer.Node, as: Node
+
+Logger.info "Node.Status"
+
+argv = System.argv()
+Logger.debug "argv=#{inspect argv}"
+
+switches_spec = [enabled: :boolean, client_id: :string, node: :string]
+parsed_args = OptionParser.parse!(argv, strict: switches_spec)
+Logger.debug "parsed_args=#{inspect parsed_args}"
+
+case parsed_args do
+  {[enabled: enabled, client_id: client_id, node: hostname], []} ->
+    Logger.debug "enabled=#{enabled}; client_id=#{client_id}; node=#{hostname};"
+
+    credential = HMServer.Credential.get_by!(client_id)
+    node = HMServer.Node.get_by!(hostname, credential)
+    node
+    |> Ecto.Changeset.change(disabled: !enabled)
+    |> HMServer.Repo.update!
+
+    Logger.info "Success!!"
+  _ ->
+    Logger.error "Received Invalid Arguments!!"
+    exit(1)
+end
