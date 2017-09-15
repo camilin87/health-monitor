522c946e37005b6f503ca928b0e38cf725abeb66
Camilo Sanchez <camilin87@gmail.com>
2017-09-14 22:34:39 -0400

    properly read the arguments to the create credentials script

--- priv/ops/create_credential.exs
+++ priv/ops/create_credential.exs
@@ -1,5 +1,33 @@
 # Script to create a Credential
 # It can be run like this
-#    mix run priv/ops/create_credential.exs
+#    mix run priv/ops/create_credential.exs --client-id "aaaaa" --secret "super secret"
 #
-IO.puts "Creating credential"
+
+require Logger
+
+defmodule OpsHelper do
+  def abort_execution(message) do
+    Logger.error message
+    exit(1)
+  end
+end
+
+Logger.info "Create Credential"
+
+
+argv = System.argv()
+Logger.debug "argv=#{inspect argv}"
+
+
+switches_spec = [client_id: :string, secret: :string]
+parsed_args = OptionParser.parse!(argv, strict: switches_spec)
+Logger.debug "parsed_args=#{inspect parsed_args}"
+
+
+case parsed_args do
+  {[client_id: client_id, secret: secret], []} ->
+    Logger.debug "client_id=#{client_id}; secret=#{secret};"
+  _ -> OpsHelper.abort_execution("Received Invalid Arguments!!")
+end
+
+
