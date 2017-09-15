c7f79243cc5a5c60976fdf6ce1b74ff54477c031
Camilo Sanchez <camilin87@gmail.com>
2017-09-15 08:21:46 -0400

    create a script to update a credential secret

---
+++ priv/ops/credential_update.exs
@@ -0,0 +1,30 @@
+# Script to create a Credential
+# It can be run like this
+#    mix ops.credential.update --client-id "aaaaa" --secret "super secret"
+#
+
+require Logger
+
+Logger.info "Credential.Update"
+
+argv = System.argv()
+Logger.debug "argv=#{inspect argv}"
+
+switches_spec = [client_id: :string, secret: :string]
+parsed_args = OptionParser.parse!(argv, strict: switches_spec)
+Logger.debug "parsed_args=#{inspect parsed_args}"
+
+case parsed_args do
+  {[client_id: client_id, secret: secret], []} ->
+    Logger.debug "client_id=#{client_id}; secret=#{secret};"
+
+    client_id
+    |> HMServer.Credential.get_by!
+    |> Ecto.Changeset.change(secret_key: secret)
+    |> HMServer.Repo.update!
+
+    Logger.info "Success!!"
+  _ ->
+    Logger.error "Received Invalid Arguments!!"
+    exit(1)
+end
